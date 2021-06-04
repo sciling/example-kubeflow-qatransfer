cb650a3ede35a73c8f36df39c3a008b4ac7657ff
victoria <vbeltran@sciling.com>
2021-06-04 12:03:08 +0200

    wikiqa pipeline

---
+++ src/wikiqa/download.py
@@ -0,0 +1,60 @@
+from kfp.components import OutputPath
+
+
+def download(squad_url, dataset_path: OutputPath()):
+    import os
+    import tempfile
+    import zipfile
+
+    import requests
+
+    from tqdm import tqdm
+
+    # Download WikiQA
+    r = requests.get(
+        "https://download.microsoft.com/download/E/5/F/E5FCFCEE-7005-4814-853D-DAA7C66507E0/WikiQACorpus.zip",
+        stream=True,
+    )
+    total_size_in_bytes = int(r.headers.get("content-length", 0))
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with tempfile.TemporaryFile() as tf:
+        for chunk in r.iter_content(chunk_size=128):
+            progress_bar.update(len(chunk))
+            tf.write(chunk)
+        with zipfile.ZipFile(tf, "r") as f:
+            f.extractall(dataset_path)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+    # Download GloVe
+    GLOVE_DIR = dataset_path + "/glove"
+    os.makedirs(GLOVE_DIR, exist_ok=True)
+    r = requests.get("http://nlp.stanford.edu/data/glove.6B.zip", stream=True)
+    total_size_in_bytes = int(r.headers.get("content-length", 0))
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with tempfile.TemporaryFile() as tf:
+        for chunk in r.iter_content(chunk_size=1024):
+            progress_bar.update(len(chunk))
+            tf.write(chunk)
+        with zipfile.ZipFile(tf, "r") as f:
+            f.extractall(GLOVE_DIR)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+    # Download Squad
+    r_squad = requests.get(squad_url)
+    total_size_in_bytes = int(r_squad.headers.get("content-length", 0))
+    progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+    with tempfile.TemporaryFile() as tf:
+        for chunk in r_squad.iter_content(chunk_size=1024):
+            progress_bar.update(len(chunk))
+            tf.write(chunk)
+        with zipfile.ZipFile(tf, "r") as f:
+            f.extractall(dataset_path)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+    print(os.listdir(dataset_path))
---
+++ src/wikiqa/pipeline.py
@@ -0,0 +1,111 @@
+import kfp
+
+from kfp.components import func_to_container_op
+
+
+def qa_pipeline(
+    squad_url: str = "https://github.com/sciling/qatransfer/releases/download/v0.1/save_class.zip",
+    squad_load_path: str = "/save/out/squad/basic-class/00/save/basic-class-1",
+    squad_shared_path: str = "/save/out/squad/basic-class/00/shared.json",
+    run_id: str = "00",
+    sent_size_th: str = "150",
+    ques_size_th: str = "100",
+    num_epochs: str = "12",
+    num_steps: str = "5000",
+    eval_period: str = "200",
+    save_period: str = "200",
+    start_step: int = 2001,
+    end_step: int = 2201,
+):
+    from download import download
+    from wikiqa_evaluate import wikiqa_evaluate
+    from wikiqa_prepro import prepro_class
+    from wikiqa_test import wikiqa_test
+    from wikiqa_train import wikiqa_train
+
+    download_op = func_to_container_op(
+        download,
+        base_image="tensorflow/tensorflow:latest-gpu-py3",
+        packages_to_install=["tqdm"],
+    )
+
+    wikiqa_prepro_op = func_to_container_op(
+        prepro_class,
+        base_image="sciling/tensorflow:0.12.0-py3",
+        packages_to_install=[
+            "https://github.com/sciling/qatransfer/archive/refs/heads/master.zip#egg=qatransfer"
+        ],
+    )
+
+    train_op = func_to_container_op(
+        wikiqa_train,
+        base_image="sciling/tensorflow:0.12.0-py3",
+        packages_to_install=[
+            "https://github.com/sciling/qatransfer/archive/refs/heads/master.zip#egg=qatransfer"
+        ],
+    )
+
+    evaluate_op = func_to_container_op(
+        wikiqa_evaluate,
+        base_image="sciling/tensorflow:0.12.0-py3",
+        packages_to_install=[
+            "https://github.com/sciling/qatransfer/archive/refs/heads/master.zip#egg=qatransfer"
+        ],
+    )
+
+    test_op = func_to_container_op(
+        wikiqa_test,
+        base_image="sciling/tensorflow:0.12.0-py3",
+        packages_to_install=[
+            "https://github.com/sciling/qatransfer/archive/refs/heads/master.zip#egg=qatransfer"
+        ],
+    )
+
+    # Download
+    dataset_path = download_op(squad_url)
+
+    # Preprocess wikiqa
+    wikiqa_prepro = wikiqa_prepro_op(dataset_path.output)
+
+    # Train wikiqa with pretrained model SQUAD
+    trained_model = train_op(
+        dataset_path.output,
+        wikiqa_prepro.output,
+        load_path=squad_load_path,
+        shared_path=squad_shared_path,
+        run_id=run_id,
+        sent_size_th=sent_size_th,
+        ques_size_th=ques_size_th,
+        num_epochs=num_epochs,
+        num_steps=num_steps,
+        eval_period=eval_period,
+        save_period=save_period,
+    )
+
+    evaluate_op(
+        wikiqa_prepro.output,
+        trained_model.output,
+        start_step,
+        end_step,
+        eval_period,
+        run_id,
+    )
+
+    test_op(
+        dataset_path.output,
+        wikiqa_prepro.output,
+        trained_model.output,
+        shared_path=squad_shared_path,
+        run_id=run_id,
+        sent_size_th=sent_size_th,
+        ques_size_th=ques_size_th,
+        num_epochs=num_epochs,
+        num_steps=num_steps,
+        eval_period=eval_period,
+        save_period=save_period,
+    )
+
+
+if __name__ == "__main__":
+    # Compile pipeline to generate compressed YAML definition of the pipeline.
+    kfp.compiler.Compiler().compile(qa_pipeline, "{}.zip".format("wikiqa_pipeline"))
