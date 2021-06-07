3a64d15510782db31d96182a2fce1aef58c67b1d
victoria <vbeltran@sciling.com>
2021-06-07 12:43:14 +0200

    modified test in semeval

--- tests/semeval/test_semeval_test.py
+++ tests/semeval/test_semeval_test.py
@@ -14,32 +14,33 @@ WORK_DIR = tempfile.mkdtemp()
 
 class TestAll(TestCase):
     def test_test(self):
-        import sys
-
-        from io import StringIO
+        import json
 
         from src.semeval.semeval_test import semeval_test
 
-        try:
-            capturedOutput = StringIO()
-            sys.stdout = capturedOutput
-            semeval_test(
-                DATA_DIR + "/generated_files",
-                "00",
-                "2001",
-                "2002",
-                "10",
-                "10",
-                "trec",
-                "False",
-                "Flase",
-            )
-        finally:
-            sys.stdout = sys.__stdout__
-        print(capturedOutput.getvalue())
-        self.assertIn("MAP", capturedOutput.getvalue())
-        self.assertIn("MRR", capturedOutput.getvalue())
-        self.assertIn("AvgRec", capturedOutput.getvalue())
+        mlpipeline_metrics_path = tempfile.NamedTemporaryFile()
+
+        semeval_test(
+            DATA_DIR + "/generated_files",
+            "00",
+            "2001",
+            "2002",
+            "10",
+            "10",
+            "trec",
+            "False",
+            "Flase",
+            mlpipeline_metrics_path.name,
+        )
+
+        with open(mlpipeline_metrics_path.name, "r") as f:
+            metrics = json.load(f)
+        self.assertIn("metrics", list(metrics.keys()))
+        self.assertEqual(3, len(list(metrics["metrics"])))
+        self.assertEqual("MAP-test-00-002001", metrics["metrics"][0]["name"])
+        self.assertEqual("MRR-test-00-002001", metrics["metrics"][1]["name"])
+        self.assertEqual("AvgRec-test-00-002001", metrics["metrics"][2]["name"])
+        mlpipeline_metrics_path.close()
 
 
 if __name__ == "__main__":
